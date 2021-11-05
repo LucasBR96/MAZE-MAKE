2be15c426408ee2b05b552e1948dc2ad9d9547ef
LucasBR96 <lucasfuzatocipriano@hotmail.com>
2021-11-05 11:54:55 -0300

    começando a criar os algoritimos

---
+++ algos.py
@@ -0,0 +1,17 @@
+from data_structures import GRAPH , FCD , MIN_HEAP
+import sys
+
+def DFS( V , E ):
+
+    
+    yield None
+    
+def WFS( V , E ):
+    yield None
+    
+def Kruskal( V , E ):
+    yield None
+    
+def Prim( V , E ):
+    yield None
+    
