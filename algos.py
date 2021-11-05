dedb7aff277537cecf9158e8663fc5ad1c5aa5b7
LucasBR96 <lucasfuzatocipriano@hotmail.com>
2021-11-05 12:11:25 -0300

    DFS done

--- algos.py
+++ algos.py
@@ -1,10 +1,31 @@
 from data_structures import GRAPH , FCD , MIN_HEAP
 import sys
 
-def DFS( V , E ):
+def DFS( G ):
 
-    
-    yield None
+    A = G.Adj_tab()
+    for u in G.nodes:
+        A[ u ].sort( key = lambda v : G.edge_val( u , v ) )
+
+    pivot = G.rand_node()
+    unvisited = G.nodes - { pivot }
+
+    stack = [ pivot ]
+    while stack:
+
+        x = stack[ -1 ]
+        if not A[ x ]:
+            stack.pop()
+            continue
+        
+        y = A[ x ].pop()
+        if y not in unvisited:
+            continue
+
+        stack.append( y )
+        unvisited.discard( y )
+
+        yield ( x , y )
     
 def WFS( V , E ):
     yield None
